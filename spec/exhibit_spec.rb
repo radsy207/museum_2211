f35b8ce965074ae8f5036d1183b96b703ff120e6
David Marino <davidjmarino8@gmail.com>
2023-01-03 11:06:01 -0500

    Initial Commit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,12 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+  let(exhibit) do 
+    Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(exhibit).to be_an_instance_of(Exhibit)
+  end
 end
