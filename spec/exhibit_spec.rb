14a2a17cb008587ace1c3059ea0aa2539119852c
David Marino <davidjmarino8@gmail.com>
2023-01-03 11:10:57 -0500

    further tested initialize

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Exhibit do
     it 'exists' do
       expect(exhibit).to be_an_instance_of(Exhibit)
     end
+
+    it 'has a name' do
+      expect(exhibit.name).to eq("Gems and Minerals")
+    end
+
+    it 'has a cost of admission' do
+      expect(exhibit.cost).to eq(0)
+    end
   end
 end
