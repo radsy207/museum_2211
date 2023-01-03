63035781d78fc6239f53a82ab98929bee347d2cd
David Marino <davidjmarino8@gmail.com>
2023-01-03 11:09:04 -0500

    exhibit initialized

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,12 +2,13 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  let(exhibit) do 
+  let(:exhibit) do 
     Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
   describe '#initialize' do
     it 'exists' do
       expect(exhibit).to be_an_instance_of(Exhibit)
+    end
   end
 end
