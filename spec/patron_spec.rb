3786533eb4fe28715dc90eb7451ce6df5bd95446
David Marino <davidjmarino8@gmail.com>
2023-01-03 11:15:54 -0500

    Patron initialize test created

---
+++ spec/patron_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/patron'
+
+RSpec.describe Patron do
+  let(:patron_1) {Patron.new("Bob", 20)}
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(patron_1).to be_an_instance_of(Patron)
+    end
+  end
+end
