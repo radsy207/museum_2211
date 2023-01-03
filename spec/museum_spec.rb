273a27e11c8ce7d8322e36c0eefe52713265c3c7
David Marino <davidjmarino8@gmail.com>
2023-01-03 11:43:14 -0500

    add exhibits method test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -28,5 +28,14 @@ RSpec.describe Museum do
     end
   end
 
+  describe '#add_exhibits' do
+    it 'adds exhibits to empty array' do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end
+
 
 end
